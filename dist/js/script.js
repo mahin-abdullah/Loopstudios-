eb4debaf7f42a688e98e95a6a0365b2516af1210
Mahin <mahinabdulah@gmail.com>
2021-04-16 23:05:24 +0600

    Added the featured section for mobile screens.

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,8 +5,10 @@ Script for toggle mobile menu
 window.onload = function () {
 
   const menu_btn = document.querySelector('.hamburger');
+  const mobile_menu = document.querySelector('.mobile__nav');
 
   menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
   });
 }
